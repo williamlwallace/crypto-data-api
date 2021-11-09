8a3395b0df4573210e8a138ce9c182d024d4853f
William <williamwallace424@yahoo.co.nz>
2021-11-09 17:54:07 +1300

    port updates

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${3000}`);
+  console.log(`Server listening on port ${port}`);
 })
 
 app.get("/crypto/:symbol", async (req, res) => {
